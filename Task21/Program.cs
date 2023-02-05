888d9fa60d4e73bf4f4656b1017609beb7e199fb
Denis <Den00736@mail.ru>
2023-02-05 20:58:34 +0400

    заливаем задания на GitHub

---
+++ Task21/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 21
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+Console.Clear();
+
+Console.WriteLine("Введите координаты точки A: ");
+Console.Write("X: ");
+int x1 = int.Parse(Console.ReadLine()!);
+Console.Write("Y: ");
+int y1 = int.Parse(Console.ReadLine()!);
+Console.Write("Z: ");
+int z1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите координаты точки B: ");
+Console.Write("X: ");
+int x2 = int.Parse(Console.ReadLine()!);
+Console.Write("Y: ");
+int y2 = int.Parse(Console.ReadLine()!);
+Console.Write("Z: ");
+int z2 = int.Parse(Console.ReadLine()!);
+
+
+
+double x3 = Math.Pow(x1 - x2, 2);
+double y3 = Math.Pow(y1 - y2, 2);
+double z3 = Math.Pow(z1 - z2, 2);
+
+double C = Math.Sqrt(x3 + y3 + z3);
+
+Console.WriteLine(Math.Round(C, 2)); // 2 способа вывода двух знаков после запятой
+Console.WriteLine($"{C:f2}");
---
+++ Task23/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+int count = 1;
+
+while (count<=N)
+{
+    double N3 = Math.Pow(count, 3);
+    Console.Write(N3);
+    if (count != N)
+    {
+        Console.Write(", ");
+    }
+    count++;
+}
